46e5a10a4bc395d400432b84d8112e0780b815eb
rajpola17 <rajpola@gmail.com>
2017-06-30 14:32:21 -0400

    Added For loop

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,9 @@ public class HelloWorld {
 
 	public static void main (String  []	args) {
 	System.out.println ("Welcome to java");
+		
+	for (Int i=0 ; i>10;  i++) {
+	System.out.println ("Welcome to java");	
 	}
+    }
 }
